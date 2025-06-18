12cfcb35604d86f6c63ae14a1d68a75f54b2a161
Christopher Lucas <christophersalomao5@gmail.com>
2025-06-18 15:22:00 -0300

    projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// menu
+
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav");
 
@@ -7,4 +9,26 @@ function closeMenu() {
 
 hamburger.addEventListener("click", () => {
   navList.classList.toggle("active");
+});
+
+
+// scroll suave
+
+const links = document.querySelectorAll(".js-link");
+
+
+function scrollSection(event) {
+  event.preventDefault();
+
+  const href = event.currentTarget.getAttribute("href");
+  const actuallySection = document.querySelector(href);
+  const sectionTop = actuallySection.offsetTop - 100;
+  window.scrollTo({
+    top: sectionTop,
+    behavior: "smooth",
+  });
+}
+
+links.forEach((link) => {
+  link.addEventListener("click", scrollSection);
 });
